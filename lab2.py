f7f294e28496674367dd41c618d950b2350e76bd
DawidJedraszczyk <dawidjedraszczyk01@gmail.com>
2024-10-11 18:45:15 +0200

    simple fix (added calculation functions)

--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
 import math
 from collections import Counter
 
+
 class DecisionTreeInductor:
     def __init__(self, data: dict = None):
         """
@@ -12,66 +13,88 @@ class DecisionTreeInductor:
             'med': ['yes', 'no'],
             'high': ['yes', 'yes', 'yes', 'no', 'yes', 'no']
         }
+        self.decisions = []  # no, no, yes, no, ...
+        self.keys = []  # low, med, high
 
-        # Flatten decisions and initialize variables
-        self.decisions = [decision for decision_list in self.data.values() for decision in decision_list]
-        self.decision_counts = Counter(self.decisions)  # Example: {'yes': 5, 'no': 5}
-        self.total_decisions = sum(self.decision_counts.values())  # Total number of decisions
+        for decision_list in self.data.values():
+            self.decisions.extend(decision_list)
 
-        # Calculate key probabilities
+        for key in self.data.keys():
+            self.keys.extend(key)
+
+        self.decision_counts = Counter(self.decisions)  # no = 5 yes = 5
+        self.total_decisions = self.total_decisions = sum(decision for decision in self.decision_counts.values())  # 10
         self.keys_probability = self._count_keys_probability()
+        print(self.keys_probability)
 
     def run(self):
         entropy_results = {}
 
-        # Overall entropy for all decisions
+        # entropy for all decisions
         entropy_results['entropy_value'] = self._entropy_calculation(**self.decision_counts)
 
-        # Entropy for decisions by key
+        # entropy for decisions for one key
         for key, decision_list in self.data.items():
             decision_counts = Counter(decision_list)
-            entropy_results[f'entropy_{key}'] = self._entropy_calculation(**decision_counts)
+            entropy_value = self._entropy_calculation(**decision_counts)
 
-        # Conditional entropy calculation
-        entropy_results['conditional_entropy'] = sum(
-            (len(decision_list) / self.total_decisions) * entropy_results[f'entropy_{key}']
-            for key, decision_list in self.data.items()
-        )
+            # Dynamically create a key for each entropy result
+            entropy_results[f'entropy_{key}'] = entropy_value
 
-        # Information gain, intrinsic information, and gain ratio
-        entropy_results['information_gain'] = self._information_gain_calculation(
-            entropy_results['entropy_value'], entropy_results['conditional_entropy']
-        )
-        entropy_results['intrinsic_info'] = self._entropy_calculation(**self.keys_probability)
-        entropy_results['gain_ratio'] = self._gain_ratio_calculation(
-            entropy_results['information_gain'], entropy_results['intrinsic_info']
-        )
+        conditional_entropy = 0
+        for key, decision_list in self.data.items():
+            decision_size = len(decision_list)
+            probability = decision_size / self.total_decisions
+            conditional_entropy += (probability * entropy_results[f'entropy_{key}'])
+        entropy_results['conditional_entropy'] = conditional_entropy
 
-        # Display results
+        entropy_results['information_gain'] = self._information_gain_calculation(entropy_results['entropy_value'],
+                                                                                 entropy_results['conditional_entropy'])
+
+        entropy_results['intrinsic_info'] = self._intrinsic_info_calculation(**self.keys_probability)
+
+        entropy_results['gain_ratio'] = self._gain_ratio_calculation(entropy_results['information_gain'],
+                                                                     entropy_results['intrinsic_info'])
+
         print(entropy_results)
 
     def _entropy_calculation(self, **kwargs) -> float:
         """
-        Calculate entropy given the counts of decision outcomes.
-        :param kwargs: Example: yes=2, no=3
-        :return: Entropy value
+        :param kwargs
+        ex. yes= 2, no=2, maybe=3
+        :return entropy value -> float
+        ex. 1.557
         """
-        total = sum(kwargs.values())
-        return -sum((count / total) * math.log2(count / total) for count in kwargs.values() if count > 0)
+        all_items = sum(value for value in kwargs.values())
+        return -sum(value / all_items * math.log2(value / all_items) for value in kwargs.values())
 
-    def _information_gain_calculation(self, entropy_value: float, conditional_entropy_value: float) -> float:
-        return entropy_value - conditional_entropy_value
+    def _conditional_entropy_calculation(self, *args) -> float:
+        """
+        :param args as lists
+        ex. [0.2, 0], [0.2, 1], [0.6, 0.918]
+        :return conditional entropy value -> float
+        ex. 0.7508
+        """
+        return sum(probability * entropy for probability, entropy in args)
 
-    def _gain_ratio_calculation(self, information_gain_value: float, intrinsic_info_value: float) -> float:
-        return information_gain_value / intrinsic_info_value if intrinsic_info_value != 0 else 0
+    def _information_gain_calculation(self, entropy_calculation_value: float,
+                                      conditional_entropy_calculation_value: float) -> float:
+        """
+        :param args -> result of entropy_calculation -> float, conditional_entropy_calculation -> float:
+        :return information gain value -> float
+        """
+        return entropy_calculation_value - conditional_entropy_calculation_value
+
+    def _intrinsic_info_calculation(self, **kwargs):
+        return self._entropy_calculation(**kwargs)
+
+    def _gain_ratio_calculation(self, information_gain_calculation_value: float,
+                                intrinsic_info_calculation_value: float) -> float:
+        return information_gain_calculation_value / intrinsic_info_calculation_value
 
     def _count_keys_probability(self):
-        """
-        Calculate the probability of each key based on the number of decisions.
-        :return: A dictionary of key probabilities
-        """
         return {key: len(decision_list) / self.total_decisions for key, decision_list in self.data.items()}
 
-# Example usage
+
 dec = DecisionTreeInductor()
 dec.run()
