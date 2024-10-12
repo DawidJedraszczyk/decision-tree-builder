747e4dea7594955d8a1963b78f49a900503c6de2
DawidJedraszczyk <dawidjedraszczyk01@gmail.com>
2024-10-12 13:27:22 +0200

    wersja z wizualizacją

--- lab2.py
+++ lab2.py
@@ -1,22 +1,15 @@
 import math
 from collections import Counter
 import pandas as pd
-
+import graphviz
+import pydotplus
 
 
 class DecisionTreeInductor:
-    def __init__(self, data: dict = None):
-        """
-        :param data:
-        ex. {'low': ['no', 'no'], 'med': ['yes', 'no'], 'high': ['yes', 'yes', 'yes', 'no', 'yes', 'no']}
-        """
-        self.data = data or {
-            'low': ['no', 'no'],
-            'med': ['yes', 'no'],
-            'high': ['yes', 'yes', 'yes', 'no', 'yes', 'no']
-        }
-        self.decisions = []  # no, no, yes, no, ...
-        self.keys = []  # low, med, high
+    def __init__(self, data: dict = None, target_attribute: str = 'Survived'):
+        self.data = data
+        self.decisions = []
+        self.keys = []
 
         for decision_list in self.data.values():
             self.decisions.extend(decision_list)
@@ -24,10 +17,10 @@ class DecisionTreeInductor:
         for key in self.data.keys():
             self.keys.append(key)
 
-        self.decision_counts = Counter(self.decisions)  # no = 5 yes = 5
-        self.total_decisions = self.total_decisions = sum(decision for decision in self.decision_counts.values())  # 10
+        self.decision_counts = Counter(self.decisions)
+        self.total_decisions = sum(self.decision_counts.values())
         self.keys_probability = self._count_keys_probability()
-        print(self.keys_probability)
+        self.target_attribute = target_attribute
 
     def run(self):
         entropy_results = {}
@@ -52,42 +45,67 @@ class DecisionTreeInductor:
 
         entropy_results['conditional_entropy'] = conditional_entropy
 
-        entropy_results['information_gain'] = self._information_gain_calculation(entropy_results['entropy_value'],
-                                                                                 entropy_results['conditional_entropy'])
+        entropy_results['information_gain'] = self._information_gain_calculation(
+            entropy_results['entropy_value'],
+            entropy_results['conditional_entropy']
+        )
 
-        entropy_results['intrinsic_info'] = self._intrinsic_info_calculation(**{str(k): v for k, v in self.keys_probability.items()})
+        entropy_results['intrinsic_info'] = self._intrinsic_info_calculation(
+            **{str(k): v for k, v in self.keys_probability.items()}
+        )
 
-        entropy_results['gain_ratio'] = self._gain_ratio_calculation(entropy_results['information_gain'],
-                                                                     entropy_results['intrinsic_info'])
+        entropy_results['gain_ratio'] = self._gain_ratio_calculation(
+            entropy_results['information_gain'],
+            entropy_results['intrinsic_info']
+        )
 
-        print(entropy_results)
-        print("")
+        return entropy_results
+
+    def build_tree(self, data, attributes, target_attribute):
+        # Check if all decisions are the same
+        decisions = data[target_attribute]
+        if len(set(decisions)) == 1:
+            return decisions.iloc[0]  # Return the decision (leaf node)
+
+        # If no more attributes to split on, return the majority class
+        if len(attributes) == 0:
+            return decisions.mode()[0]  # Majority class
+
+        # Find the best attribute by gain ratio
+        results = {}
+        for attribute in attributes:
+            data_for_attribute = data[[attribute, target_attribute]]
+            grouped_data = data_for_attribute.groupby(attribute)[target_attribute].apply(list).to_dict()
+            dti = DecisionTreeInductor(data=grouped_data)
+            result = dti.run()
+            results[attribute] = result
+
+        # Select the attribute with the highest gain ratio
+        sorted_data = dict(sorted(results.items(), key=lambda item: item[1]['gain_ratio'], reverse=True))
+        best_attribute = next(iter(sorted_data))
+
+        # Create a node with the best attribute
+        tree = {best_attribute: {}}
+
+        # Split the data based on the best attribute and build the subtree recursively
+        attribute_values = data[best_attribute].unique()
+        for value in attribute_values:
+            subset_data = data[data[best_attribute] == value]
+            subtree = self.build_tree(subset_data, [attr for attr in attributes if attr != best_attribute],
+                                      target_attribute)
+            tree[best_attribute][value] = subtree
+
+        return tree
 
     def _entropy_calculation(self, **kwargs) -> float:
-        """
-        :param kwargs
-        ex. yes= 2, no=2, maybe=3
-        :return entropy value -> float
-        ex. 1.557
-        """
         all_items = sum(value for value in kwargs.values())
         return -sum(value / all_items * math.log2(value / all_items) for value in kwargs.values())
 
     def _conditional_entropy_calculation(self, *args) -> float:
-        """
-        :param args as lists
-        ex. [0.2, 0], [0.2, 1], [0.6, 0.918]
-        :return conditional entropy value -> float
-        ex. 0.7508
-        """
         return sum(probability * entropy for probability, entropy in args)
 
     def _information_gain_calculation(self, entropy_calculation_value: float,
                                       conditional_entropy_calculation_value: float) -> float:
-        """
-        :param args -> result of entropy_calculation -> float, conditional_entropy_calculation -> float:
-        :return information gain value -> float
-        """
         return entropy_calculation_value - conditional_entropy_calculation_value
 
     def _intrinsic_info_calculation(self, **kwargs):
@@ -95,19 +113,63 @@ class DecisionTreeInductor:
 
     def _gain_ratio_calculation(self, information_gain_calculation_value: float,
                                 intrinsic_info_calculation_value: float) -> float:
-        return information_gain_calculation_value / intrinsic_info_calculation_value
+        try:
+            return information_gain_calculation_value / intrinsic_info_calculation_value
+        except ZeroDivisionError:
+            return 0
 
     def _count_keys_probability(self):
         return {key: len(decision_list) / self.total_decisions for key, decision_list in self.data.items()}
+
+def visualize_tree(tree, parent_name='', graph=None):
+    if graph is None:
+        graph = graphviz.Digraph(format='png')
+
+    for node, branches in tree.items():
+        node_name = node
+        graph.node(node_name)
+
+        if parent_name:
+            graph.edge(parent_name, node_name)
+
+        if isinstance(branches, dict):
+            # Recursively draw branches
+            for branch, subtree in branches.items():
+                branch_name = f"{node_name}_{branch}"
+                graph.node(branch_name, label=str(branch))
+                graph.edge(node_name, branch_name)
+
+                if isinstance(subtree, dict):
+                    visualize_tree(subtree, branch_name, graph)
+                else:
+                    # Leaf node
+                    leaf_name = f"{branch_name}_leaf"
+                    graph.node(leaf_name, label=str(subtree), shape='box')
+                    graph.edge(branch_name, leaf_name)
+
+    return graph
 
 if __name__ == "__main__":
     data = pd.read_csv("data/titanic-homework.csv")
     attributes = [col for col in data.columns if col not in ['Survived', 'PassengerId', 'Name']]
 
-    for attribute in attributes:
-        data_for_attribute = data[[attribute, 'Survived']]
-        grouped_data = data_for_attribute.groupby(attribute)['Survived'].apply(list).to_dict()  # Convert to dict
+    # Create the DecisionTreeInductor with data
+    grouped_data = data.groupby(attributes[0])['Survived'].apply(list).to_dict()
+    dti = DecisionTreeInductor(data=grouped_data)
 
-        print(attribute)
-        dti = DecisionTreeInductor(data=grouped_data)  # Pass as a dict
-        dti.run()
+    decision_tree = dti.build_tree(data, attributes, 'Survived')
+
+    graph = visualize_tree(decision_tree)
+    graph.render('decision_tree', view=True)
+
+
+
+
+
+
+
+
+
+
+
+
